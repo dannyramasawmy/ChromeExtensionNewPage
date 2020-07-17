c35c8f6c2b3d08aca7f4377a410b9a32ebace974
Danny Ramasawmy <rmapdrr@ucl.ac.uk>
2020-07-17 17:21:14 +0100

    Visual update.

--- suggestions.js
+++ suggestions.js
@@ -31,8 +31,8 @@ function formatADT() {
     // Format date/time string.
     var dateString =  "" 
         + (("0"+dt.getDate()).slice(-2)) 
-        +":"+ (("0"+(dt.getMonth()+1)).slice(-2)) 
-        +":"+ (dt.getFullYear()) +"       |   "
+        +"-"+ (("0"+(dt.getMonth()+1)).slice(-2)) 
+        +"-"+ (dt.getFullYear()) +"       |   "
         + (("0"+dt.getHours()).slice(-2)) +":"
         + (("0"+dt.getMinutes()).slice(-2));
     // Return the date string.
